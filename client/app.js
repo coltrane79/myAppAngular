d787e6487e0e4e79a9be22443c6e502a8ab1fb40
Andrew Tattersall <atattersall@hotmail.com>
2016-08-13 19:48:09 -0400

    bundle workstream set

--- client/app.js
+++ client/app.js
@@ -1,3 +1,20 @@
-require('angular')
+var angular = require('angular');
 
-angular.module('demoApp',[]);
+var app = angular.module('demoApp',[]);
+
+var person = {
+  name: "Andrew",
+  last: "Tattersall",
+  DOB: "1979/05/21",
+  gender: "Male"
+};
+
+app.controller('appController', function(){
+  this.people = person;
+  this.calcAge = function(){
+    var currDate = new Date();
+    var bDate = new Date(people.DOB);
+    var age = currDate - bDate;
+    return age;
+  }
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,29 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var browserify = require('browserify');
+var source = require('vinyl-source-stream')
 var babelify = require('babelify');
 var fs = require('fs');
+//var jshing = require('gulp-jshint');
 
 
 gulp.task('default', function(){
-  return gulp.src('./client/*.js')
-  .pipe(concat('bundle.js'))
+  return browserify()
+  .add('./client/app.js')
+  .bundle()
+  .pipe(source('bundle.js'))
   .pipe(gulp.dest('./client/dist'));
+  //return gulp.src('./client/*.js')
+  //.pipe(concat('bundle.js'))
+  //.pipe(gulp.dest('./client/dist'));
 });
 
+//gulp.task('lint', function(){
+//  return gulp.src('./client/app.js')
+//  .pipe(jshint())
+//  .pipe(jshint.reporter('default'));
+//});
+
 gulp.task('watch', function(){
-  gulp.watch('./client/*.js',['default']);
+  gulp.watch(['./client/*.js', './client/node_modules/*'],['default']);
 });
